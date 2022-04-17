ac6191a8b1c6d36fcf658b736b7e3d4fd1261670
arora <aroramadhav65@gmail.com>
2022-04-17 20:08:19 +0530

    Update

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:plant_app/components/my_bottom_nav_bar.dart';
+import 'package:plant_app/constants.dart';
 import 'package:plant_app/screens/home/components/body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -15,6 +16,7 @@ class HomeScreen extends StatelessWidget {
 
   AppBar buildAppBar() {
     return AppBar(
+      backgroundColor: kPrimaryColor,
       elevation: 0,
       leading: IconButton(
         icon: SvgPicture.asset("../assets/icons/menu.svg"),
